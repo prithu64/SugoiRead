fdb186788a94e94805545805f1a3bced307a374e
prithu64 <prgthemessenger@gmail.com>
2025-08-08 13:39:25 +0530

    updt

--- src/pages/Manga.jsx
+++ src/pages/Manga.jsx
@@ -1,20 +1,50 @@
+import { useState } from "react"
 import { mangaData } from "../../data/mangaData"
 import MangaCard from "../components/MangaCard"
 
 function Manga() {
+  const [visibleManga,setVisible] = useState(4);
+  const loadManga = ()=>{
+    setVisible((prev)=>prev+4);
+  }
+
+  const hideManga=()=>{
+    setVisible((prev)=>prev-4)
+  }
+
   return (
     <div className='flex flex-col justify-center items-center py-[15px]'>
   
      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
         {
-         mangaData.map((manga)=>{
+         mangaData.slice(0,visibleManga).map((manga)=>{
         return(
             <MangaCard key={manga.id} src={manga.imageUrl} title={manga.title} />
         )
       })
         }
+        
      </div>
-    
+
+     <div className="flex gap-x-4">
+         {
+          visibleManga<mangaData.length &&(
+            <div>
+              <button className="flex justify-center mt-10 border p-1 w-[100px] rounded-md text-xs" onClick={loadManga}>
+               Show More
+             </button> 
+            </div> 
+          )
+        }
+         {
+              visibleManga > 4 && (
+                <button onClick={hideManga} className="flex justify-center mt-10 border p-1 w-[100px] rounded-md text-xs" >
+                  Show less
+                </button>
+              )
+         }
+     </div>
+     
     </div>
   )
 }
